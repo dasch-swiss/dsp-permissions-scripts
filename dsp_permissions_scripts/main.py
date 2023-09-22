fe6a65bc24cdfee45a3151847276c51b63471fde
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-09-22 11:55:26 +0200

    fix all doaps

--- dsp_permissions_scripts/main.py
+++ dsp_permissions_scripts/main.py
@@ -35,6 +35,18 @@ def main() -> None:
     host = Hosts.get_host("staging")
     shortcode = "0102"
     token = login(host)
+    fix_doaps(
+        host=host,
+        shortcode=shortcode,
+        token=token,
+    )
+
+
+def fix_doaps(
+    host: str,
+    shortcode: str,
+    token: str,
+) -> None:
     doaps = get_doaps_of_project(
         host=host,
         shortcode=shortcode,
@@ -45,12 +57,26 @@ def main() -> None:
         host=host,
         shortcode=shortcode,
     )
-    set_doaps_of_groups(
-        scope=StandardScope().READ_ONLY_FOR_GROUP_SCENARIO_TANNER,
-        groups=[BuiltinGroup.PROJECT_MEMBER],
+    group_scenario_tanner_scope = PermissionScopeElement(
+        info="http://rdfh.ch/groups/0102/oe8-uWCgS4Wl6wfOvaFGCA",
+        name="V",
+    )
+    for d in doaps:
+        d.scope.append(group_scenario_tanner_scope)
+    new_doaps = []
+    for d in doaps:
+        new_doap = update_doap_scope(
+            permission_iri=d.iri,
+            scope=d.scope,
+            host=host,
+            token=token,
+        )
+        new_doaps.append(new_doap)
+    print("\nNew DOAPs:\n=========")
+    print_doaps_of_project(
+        doaps=new_doaps,
         host=host,
         shortcode=shortcode,
-        token=token,
     )
 
 
