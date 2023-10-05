fe803dbcd154d7fa8ed9235e0ae58f2786cf775c
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-05 16:19:01 +0200

    improve path

--- dsp_permissions_scripts/utils/oap_serialize.py
+++ dsp_permissions_scripts/utils/oap_serialize.py
@@ -5,29 +5,35 @@ from typing import Literal
 from dsp_permissions_scripts.models.oap import Oap
 
 
+def _get_project_data_path(
+    shortcode: str,
+    mode: Literal["original", "modified"]
+) -> Path:
+    return Path(f"project_data/{shortcode}_OAPs_{mode}")
+
+
 def serialize_resource_oaps(
     resource_oaps: list[Oap],
     shortcode: str,
-    mode=Literal["original", "modified"],
+    mode: Literal["original", "modified"],
 ) -> None:
-    """Serialize the resource OAPs to JSON files into the folder "OAPs_project_{shortcode}_{mode}"."""
-    folder = Path(f"OAPs_project_{shortcode}_{mode}")
-    folder.mkdir(exist_ok=True)
+    """Serialize the resource OAPs to JSON files."""
+    folder = _get_project_data_path(shortcode, mode)
+    folder.mkdir(parents=True, exist_ok=True)
     for res_oap in resource_oaps:
-        filename = re.sub(r"", "_", res_oap.object_iri)
-        with open((folder / filename).with_suffix(".json"), mode="w", encoding="utf-8") as f:
-            f.write(res_oap.model_dump_json())
+        filename = re.sub(r"http://rdfh\.ch/[^/]+/", "resource_", res_oap.object_iri)
+        with open(folder / f"{filename}.json", mode="w", encoding="utf-8") as f:
+            f.write(res_oap.model_dump_json(indent=2))
 
 
 def deserialize_resource_oaps(
     shortcode: str,
     mode: Literal["original", "modified"],
 ) -> list[Oap]:
-    """Deserialize the resource OAPs from JSON files in the folder "OAPs_project_{shortcode}_{mode}"."""
-    folder = Path(f"OAPs_project_{shortcode}_{mode}")
+    """Deserialize the resource OAPs from JSON files."""
+    folder = _get_project_data_path(shortcode, mode)
     resource_oaps = []
     for file in folder.iterdir():
-        if file.suffix == ".json" and "rdfh" in file.name:
-            with open(file, mode="r", encoding="utf-8") as f:
-                resource_oaps.append(Oap.model_validate_json(f.read()))
+        with open(file, mode="r", encoding="utf-8") as f:
+            resource_oaps.append(Oap.model_validate_json(f.read()))
     return resource_oaps
