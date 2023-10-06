84885324a9ba068f1e3e802bf6b810244af226cd
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-06 10:55:08 +0200

    convert scope fields from lists to sets

--- dsp_permissions_scripts/models/scope.py
+++ dsp_permissions_scripts/models/scope.py
@@ -9,15 +9,15 @@ class PermissionScope(BaseModel):
     "Which user group gets which permissions on a resource/value?"
     """
 
-    CR: list[str | BuiltinGroup] = []
-    D: list[str | BuiltinGroup] = []
-    M: list[str | BuiltinGroup] = []
-    V: list[str | BuiltinGroup] = []
-    RV: list[str | BuiltinGroup] = []
+    CR: set[str | BuiltinGroup] = set()
+    D: set[str | BuiltinGroup] = set()
+    M: set[str | BuiltinGroup] = set()
+    V: set[str | BuiltinGroup] = set()
+    RV: set[str | BuiltinGroup] = set()
 
 
 PUBLIC = PermissionScope(
-    CR=[BuiltinGroup.PROJECT_ADMIN],
-    D=[BuiltinGroup.CREATOR, BuiltinGroup.PROJECT_MEMBER],
-    V=[BuiltinGroup.UNKNOWN_USER, BuiltinGroup.KNOWN_USER],
+    CR={BuiltinGroup.PROJECT_ADMIN},
+    D={BuiltinGroup.CREATOR, BuiltinGroup.PROJECT_MEMBER},
+    V={BuiltinGroup.UNKNOWN_USER, BuiltinGroup.KNOWN_USER},
 )
--- tests/test_scope_serialization.py
+++ tests/test_scope_serialization.py
@@ -42,22 +42,22 @@ class TestScopeSerialization(unittest.TestCase):
     ]
     scopes = [
         PermissionScope(
-            CR=[BuiltinGroup.SYSTEM_USER],
-            V=["http://www.knora.org/ontology/knora-admin#CustomGroup"],
+            CR={BuiltinGroup.SYSTEM_USER},
+            V={"http://www.knora.org/ontology/knora-admin#CustomGroup"},
         ),
         PermissionScope(
-            D=[BuiltinGroup.PROJECT_ADMIN],
-            RV=[BuiltinGroup.PROJECT_MEMBER],
+            D={BuiltinGroup.PROJECT_ADMIN},
+            RV={BuiltinGroup.PROJECT_MEMBER},
         ),
         PermissionScope(
-            M=[BuiltinGroup.PROJECT_ADMIN],
-            V=[BuiltinGroup.CREATOR, BuiltinGroup.KNOWN_USER],
-            RV=[BuiltinGroup.UNKNOWN_USER],
+            M={BuiltinGroup.PROJECT_ADMIN},
+            V={BuiltinGroup.CREATOR, BuiltinGroup.KNOWN_USER},
+            RV={BuiltinGroup.UNKNOWN_USER},
         ),
         PermissionScope(
-            CR=[BuiltinGroup.SYSTEM_ADMIN, BuiltinGroup.PROJECT_ADMIN],
-            D=[BuiltinGroup.CREATOR],
-            RV=[BuiltinGroup.UNKNOWN_USER],
+            CR={BuiltinGroup.SYSTEM_ADMIN, BuiltinGroup.PROJECT_ADMIN},
+            D={BuiltinGroup.CREATOR},
+            RV={BuiltinGroup.UNKNOWN_USER},
         ),
     ]
 
