f1145cb37de8ae4c0564a635f4c9862103237e99
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-06 16:29:35 +0200

    group ordering

--- dsp_permissions_scripts/models/groups.py
+++ dsp_permissions_scripts/models/groups.py
@@ -8,8 +8,8 @@ class BuiltinGroup(Enum):
 
     UNKNOWN_USER = "http://www.knora.org/ontology/knora-admin#UnknownUser"
     KNOWN_USER = "http://www.knora.org/ontology/knora-admin#KnownUser"
-    CREATOR = "http://www.knora.org/ontology/knora-admin#Creator"
     PROJECT_MEMBER = "http://www.knora.org/ontology/knora-admin#ProjectMember"
     PROJECT_ADMIN = "http://www.knora.org/ontology/knora-admin#ProjectAdmin"
+    CREATOR = "http://www.knora.org/ontology/knora-admin#Creator"
     SYSTEM_USER = "http://www.knora.org/ontology/knora-admin#SystemUser"
     SYSTEM_ADMIN = "http://www.knora.org/ontology/knora-admin#SystemAdmin"
