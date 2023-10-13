ad5b41170374fa65524c0ddfd8e48de52b05c4e5
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-13 08:55:27 +0200

    write tests for class Ap

---
+++ tests/test_ap.py
@@ -0,0 +1,29 @@
+import unittest
+
+from dsp_permissions_scripts.models import builtin_groups
+from dsp_permissions_scripts.models.ap import Ap, ApValue
+
+
+class TestAp(unittest.TestCase):
+    ap = Ap(
+        forGroup=builtin_groups.UNKNOWN_USER,
+        forProject="http://rdfh.ch/projects/0001",
+        hasPermissions=frozenset({ApValue.ProjectResourceCreateAllPermission, ApValue.ProjectAdminGroupAllPermission}),
+        iri="http://rdfh.ch/foo",
+    )
+
+    def test_add_permission(self):
+        self.ap.add_permission(ApValue.ProjectAdminRightsAllPermission)
+        self.assertIn(ApValue.ProjectAdminRightsAllPermission, self.ap.hasPermissions)
+
+    def test_add_permission_already_exists(self):
+        with self.assertRaises(ValueError):
+            self.ap.add_permission(ApValue.ProjectAdminGroupAllPermission)
+
+    def test_remove_permission(self):
+        self.ap.remove_permission(ApValue.ProjectAdminGroupAllPermission)
+        self.assertNotIn(ApValue.ProjectAdminGroupAllPermission, self.ap.hasPermissions)
+
+    def test_remove_permission_not_exists(self):
+        with self.assertRaises(ValueError):
+            self.ap.remove_permission(ApValue.ProjectAdminOntologyAllPermission)
