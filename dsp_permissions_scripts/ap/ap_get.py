8a436cc3e67e997b9db8422f7ca29f51a666dcd4
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2023-10-15 20:00:53 +0200

    chore: improve error handling (#48)

--- dsp_permissions_scripts/ap/ap_get.py
+++ dsp_permissions_scripts/ap/ap_get.py
@@ -4,6 +4,7 @@ from urllib.parse import quote_plus
 import requests
 
 from dsp_permissions_scripts.ap.ap_model import Ap, ApValue
+from dsp_permissions_scripts.models.api_error import ApiError
 from dsp_permissions_scripts.utils.authentication import get_protocol
 from dsp_permissions_scripts.utils.get_logger import get_logger
 from dsp_permissions_scripts.utils.project import get_project_iri_by_shortcode
@@ -47,7 +48,8 @@ def _get_all_aps_of_project(
     protocol = get_protocol(host)
     url = f"{protocol}://{host}/admin/permissions/ap/{project_iri}"
     response = requests.get(url, headers=headers, timeout=5)
-    assert response.status_code == 200, f"Status {response.status_code}. Error message from DSP-API: {response.text}"
+    if response.status_code != 200:
+        raise ApiError("Could not get APs of project", response.text, response.status_code)
     aps: list[dict[str, Any]] = response.json()["administrative_permissions"]
     ap_objects = [create_ap_from_admin_route_object(ap) for ap in aps]
     return ap_objects
