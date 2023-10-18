799dab6660ab9437c9e234bd6d7a3e404d46dad3
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2023-10-18 11:34:55 +0200

    refactor: move AP deletion to own module (#63)

--- dsp_permissions_scripts/ap/ap_set.py
+++ dsp_permissions_scripts/ap/ap_set.py
@@ -17,23 +17,6 @@ from dsp_permissions_scripts.utils.try_request import http_call_with_retry
 logger = get_logger(__name__)
 
 
-def _delete_ap_on_server(
-    ap: Ap,
-    host: str,
-    token: str,
-) -> None:
-    headers = {"Authorization": f"Bearer {token}"}
-    ap_iri = quote_plus(ap.iri, safe="")
-    protocol = get_protocol(host)
-    url = f"{protocol}://{host}/admin/permissions/{ap_iri}"
-    response = http_call_with_retry(
-        action=lambda: requests.delete(url, headers=headers, timeout=10),
-        err_msg=f"Could not delete Administrative Permission {ap.iri}",
-    )
-    if response.status_code != 200:
-        raise ApiError(f"Could not delete Administrative Permission {ap.iri}", response.text, response.status_code)
-
-
 def _update_ap_on_server(
     ap: Ap,
     host: str,
@@ -82,27 +65,3 @@ def apply_updated_aps_on_server(
         except ApiError as err:
             logger.error(err)
             warnings.warn(err.message)
-
-
-def delete_ap_of_group_on_server(
-    host: str,
-    token: str,
-    existing_aps: list[Ap],
-    forGroup: str,
-) -> list[Ap]:
-    aps_to_delete = [ap for ap in existing_aps if ap.forGroup == forGroup]
-    if not aps_to_delete:
-        logger.warning(f"There are no APs to delete on {host} for group {forGroup}")
-        warnings.warn(f"There are no APs to delete on {host} for group {forGroup}")
-        return existing_aps
-    print(f"Deleting the Administrative Permissions for group {forGroup} on server {host}")
-    logger.info(f"Deleting the Administrative Permissions for group {forGroup} on server {host}")
-    for ap in aps_to_delete:
-        _delete_ap_on_server(
-            ap=ap,
-            host=host,
-            token=token,
-        )
-        existing_aps.remove(ap)
-        logger.info(f"Deleted Administrative Permission {ap.iri} on host {host}")
-    return existing_aps
