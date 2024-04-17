0ff6797683a96500ba31cab428e5e1508041cc33
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2024-04-17 15:53:42 +0200

    test: migrate to pytest (#96)

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -1,151 +1,161 @@
-import unittest
+import re
+
+import pytest
 
 from dsp_permissions_scripts.models import group
 from dsp_permissions_scripts.models.scope import PermissionScope
 from tests.test_scope_serialization import compare_scopes
 
-# ruff: noqa: PT027 (pytest-unittest-raises-assertion) (remove this line when pytest is used instead of unittest)
-# ruff: noqa: PT009 (pytest-unittest-assertion) (remove this line when pytest is used instead of unittest)
 
-
-class TestScope(unittest.TestCase):
-    def test_scope_validation_on_creation(self) -> None:
-        with self.assertRaisesRegex(ValueError, "must not occur in more than one field"):
-            PermissionScope.create(
-                CR={group.PROJECT_ADMIN},
-                D={group.PROJECT_ADMIN},
-                V={group.UNKNOWN_USER, group.KNOWN_USER},
-            )
-
-    def test_scope_validation_on_add_to_same_permission(self) -> None:
-        scope = PermissionScope.create(
+def test_scope_validation_on_creation() -> None:
+    with pytest.raises(ValueError, match=re.escape("must not occur in more than one field")):
+        PermissionScope.create(
             CR={group.PROJECT_ADMIN},
-            V={group.UNKNOWN_USER, group.KNOWN_USER},
-        )
-        rgx = "Group 'val='http://www.knora.org/ontology/knora-admin#ProjectAdmin'' is already in permission 'CR'"
-        with self.assertRaisesRegex(ValueError, rgx):
-            _ = scope.add("CR", group.PROJECT_ADMIN)
-
-    def test_scope_validation_on_add_to_different_permission(self) -> None:
-        scope = PermissionScope.create(
-            CR={group.PROJECT_ADMIN},
-            V={group.UNKNOWN_USER, group.KNOWN_USER},
-        )
-        with self.assertRaisesRegex(ValueError, "must not occur in more than one field"):
-            _ = scope.add("RV", group.PROJECT_ADMIN)
-
-    def test_add_to_scope(self) -> None:
-        scope = PermissionScope.create(
-            D={group.SYSTEM_ADMIN},
-            M={group.PROJECT_MEMBER, group.KNOWN_USER},
-        )
-        scope_added = scope.add("CR", group.PROJECT_ADMIN)
-        compare_scopes(
-            scope1=scope_added,
-            scope2=PermissionScope.create(
-                CR={group.PROJECT_ADMIN},
-                D={group.SYSTEM_ADMIN},
-                M={group.PROJECT_MEMBER, group.KNOWN_USER},
-            ),
-        )
-
-    def test_remove_inexisting_group(self) -> None:
-        scope = PermissionScope.create(
-            D={group.SYSTEM_ADMIN},
-            M={group.PROJECT_MEMBER, group.KNOWN_USER},
-        )
-        with self.assertRaisesRegex(ValueError, "is not in permission 'D'"):
-            _ = scope.remove("D", group.UNKNOWN_USER)
-
-    def test_remove_from_empty_perm(self) -> None:
-        scope = PermissionScope.create(
             D={group.PROJECT_ADMIN},
-            V={group.PROJECT_MEMBER, group.UNKNOWN_USER},
+            V={group.UNKNOWN_USER, group.KNOWN_USER},
         )
-        with self.assertRaisesRegex(ValueError, "is not in permission 'CR'"):
-            _ = scope.remove("CR", group.PROJECT_ADMIN)
 
-    def test_remove_from_scope(self) -> None:
-        scope = PermissionScope.create(
+
+def test_scope_validation_on_add_to_same_permission() -> None:
+    scope = PermissionScope.create(
+        CR={group.PROJECT_ADMIN},
+        V={group.UNKNOWN_USER, group.KNOWN_USER},
+    )
+    rgx = "Group 'val='http://www.knora.org/ontology/knora-admin#ProjectAdmin'' is already in permission 'CR'"
+    with pytest.raises(ValueError, match=re.escape(rgx)):
+        _ = scope.add("CR", group.PROJECT_ADMIN)
+
+
+def test_scope_validation_on_add_to_different_permission() -> None:
+    scope = PermissionScope.create(
+        CR={group.PROJECT_ADMIN},
+        V={group.UNKNOWN_USER, group.KNOWN_USER},
+    )
+    with pytest.raises(ValueError, match=re.escape("must not occur in more than one field")):
+        _ = scope.add("RV", group.PROJECT_ADMIN)
+
+
+def test_add_to_scope() -> None:
+    scope = PermissionScope.create(
+        D={group.SYSTEM_ADMIN},
+        M={group.PROJECT_MEMBER, group.KNOWN_USER},
+    )
+    scope_added = scope.add("CR", group.PROJECT_ADMIN)
+    compare_scopes(
+        scope1=scope_added,
+        scope2=PermissionScope.create(
             CR={group.PROJECT_ADMIN},
             D={group.SYSTEM_ADMIN},
             M={group.PROJECT_MEMBER, group.KNOWN_USER},
-        )
-        scope_removed = scope.remove("CR", group.PROJECT_ADMIN)
-        compare_scopes(
-            scope1=scope_removed,
-            scope2=PermissionScope.create(
-                D={group.SYSTEM_ADMIN},
-                M={group.PROJECT_MEMBER, group.KNOWN_USER},
-            ),
-        )
+        ),
+    )
 
-    def test_remove_duplicates_from_kwargs_CR(self) -> None:
-        original: dict[str, list[str]] = {
-            "CR": ["knora-admin:ProjectAdmin"],
-            "D": ["knora-admin:ProjectAdmin"],
-            "M": ["knora-admin:ProjectAdmin"],
-            "V": ["knora-admin:ProjectAdmin"],
-            "RV": ["knora-admin:ProjectAdmin"],
-        }
-        expected = {"CR": ["knora-admin:ProjectAdmin"], "D": [], "M": [], "V": [], "RV": []}
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
 
-    def test_remove_duplicates_from_kwargs_D(self) -> None:
-        original: dict[str, list[str]] = {
-            "CR": [],
-            "D": ["knora-admin:ProjectAdmin"],
-            "M": ["knora-admin:ProjectAdmin"],
-            "V": ["knora-admin:ProjectAdmin"],
-            "RV": ["knora-admin:ProjectAdmin"],
-        }
-        expected = {"CR": [], "D": ["knora-admin:ProjectAdmin"], "M": [], "V": [], "RV": []}
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
+def test_remove_inexisting_group() -> None:
+    scope = PermissionScope.create(
+        D={group.SYSTEM_ADMIN},
+        M={group.PROJECT_MEMBER, group.KNOWN_USER},
+    )
+    with pytest.raises(ValueError, match=re.escape("is not in permission 'D'")):
+        _ = scope.remove("D", group.UNKNOWN_USER)
 
-    def test_remove_duplicates_from_kwargs_RV(self) -> None:
-        original: dict[str, list[str]] = {"CR": [], "D": [], "M": [], "V": [], "RV": ["knora-admin:ProjectAdmin"]}
-        expected: dict[str, list[str]] = {"CR": [], "D": [], "M": [], "V": [], "RV": ["knora-admin:ProjectAdmin"]}
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
 
-    def test_remove_duplicates_from_kwargs_mixed(self) -> None:
-        original: dict[str, list[str]] = {
-            "CR": ["knora-admin:ProjectAdmin"],
-            "D": ["knora-admin:ProjectMember"],
-            "M": ["knora-admin:ProjectMember"],
-            "V": ["knora-admin:ProjectMember"],
-            "RV": ["knora-admin:ProjectMember"],
-        }
-        expected = {"CR": ["knora-admin:ProjectAdmin"], "D": ["knora-admin:ProjectMember"], "M": [], "V": [], "RV": []}
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
+def test_remove_from_empty_perm() -> None:
+    scope = PermissionScope.create(
+        D={group.PROJECT_ADMIN},
+        V={group.PROJECT_MEMBER, group.UNKNOWN_USER},
+    )
+    with pytest.raises(ValueError, match=re.escape("is not in permission 'CR'")):
+        _ = scope.remove("CR", group.PROJECT_ADMIN)
 
-    def test_remove_duplicates_from_kwargs_mixed_M(self) -> None:
-        original: dict[str, list[str]] = {
-            "CR": ["knora-admin:ProjectAdmin"],
-            "D": [],
-            "M": ["knora-admin:ProjectMember"],
-            "V": ["knora-admin:ProjectMember"],
-            "RV": ["knora-admin:ProjectMember"],
-        }
-        expected = {"CR": ["knora-admin:ProjectAdmin"], "D": [], "M": ["knora-admin:ProjectMember"], "V": [], "RV": []}
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
 
-    def test_remove_duplicates_from_kwargs_mixed_and_multiple(self) -> None:
-        original: dict[str, list[str]] = {
-            "CR": ["knora-admin:ProjectAdmin"],
-            "D": [],
-            "M": ["knora-admin:ProjectMember", "knora-admin:ProjectAdmin", "knora-admin:KnownUser"],
-            "V": ["knora-admin:ProjectMember", "knora-admin:ProjectAdmin"],
-            "RV": ["knora-admin:ProjectMember", "knora-admin:KnownUser"],
-        }
-        expected = {
-            "CR": ["knora-admin:ProjectAdmin"],
-            "D": [],
-            "M": ["knora-admin:ProjectMember", "knora-admin:KnownUser"],
-            "V": [],
-            "RV": [],
-        }
-        self.assertDictEqual(expected, PermissionScope._remove_duplicates_from_kwargs(original))
+def test_remove_from_scope() -> None:
+    scope = PermissionScope.create(
+        CR={group.PROJECT_ADMIN},
+        D={group.SYSTEM_ADMIN},
+        M={group.PROJECT_MEMBER, group.KNOWN_USER},
+    )
+    scope_removed = scope.remove("CR", group.PROJECT_ADMIN)
+    compare_scopes(
+        scope1=scope_removed,
+        scope2=PermissionScope.create(
+            D={group.SYSTEM_ADMIN},
+            M={group.PROJECT_MEMBER, group.KNOWN_USER},
+        ),
+    )
+
+
+def test_remove_duplicates_from_kwargs_CR() -> None:
+    original: dict[str, list[str]] = {
+        "CR": ["knora-admin:ProjectAdmin"],
+        "D": ["knora-admin:ProjectAdmin"],
+        "M": ["knora-admin:ProjectAdmin"],
+        "V": ["knora-admin:ProjectAdmin"],
+        "RV": ["knora-admin:ProjectAdmin"],
+    }
+    expected = {"CR": ["knora-admin:ProjectAdmin"], "D": [], "M": [], "V": [], "RV": []}
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
+
+
+def test_remove_duplicates_from_kwargs_D() -> None:
+    original: dict[str, list[str]] = {
+        "CR": [],
+        "D": ["knora-admin:ProjectAdmin"],
+        "M": ["knora-admin:ProjectAdmin"],
+        "V": ["knora-admin:ProjectAdmin"],
+        "RV": ["knora-admin:ProjectAdmin"],
+    }
+    expected = {"CR": [], "D": ["knora-admin:ProjectAdmin"], "M": [], "V": [], "RV": []}
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
+
+
+def test_remove_duplicates_from_kwargs_RV() -> None:
+    original: dict[str, list[str]] = {"CR": [], "D": [], "M": [], "V": [], "RV": ["knora-admin:ProjectAdmin"]}
+    expected: dict[str, list[str]] = {"CR": [], "D": [], "M": [], "V": [], "RV": ["knora-admin:ProjectAdmin"]}
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
+
+
+def test_remove_duplicates_from_kwargs_mixed() -> None:
+    original: dict[str, list[str]] = {
+        "CR": ["knora-admin:ProjectAdmin"],
+        "D": ["knora-admin:ProjectMember"],
+        "M": ["knora-admin:ProjectMember"],
+        "V": ["knora-admin:ProjectMember"],
+        "RV": ["knora-admin:ProjectMember"],
+    }
+    expected = {"CR": ["knora-admin:ProjectAdmin"], "D": ["knora-admin:ProjectMember"], "M": [], "V": [], "RV": []}
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
+
+
+def test_remove_duplicates_from_kwargs_mixed_M() -> None:
+    original: dict[str, list[str]] = {
+        "CR": ["knora-admin:ProjectAdmin"],
+        "D": [],
+        "M": ["knora-admin:ProjectMember"],
+        "V": ["knora-admin:ProjectMember"],
+        "RV": ["knora-admin:ProjectMember"],
+    }
+    expected = {"CR": ["knora-admin:ProjectAdmin"], "D": [], "M": ["knora-admin:ProjectMember"], "V": [], "RV": []}
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
+
+
+def test_remove_duplicates_from_kwargs_mixed_and_multiple() -> None:
+    original: dict[str, list[str]] = {
+        "CR": ["knora-admin:ProjectAdmin"],
+        "D": [],
+        "M": ["knora-admin:ProjectMember", "knora-admin:ProjectAdmin", "knora-admin:KnownUser"],
+        "V": ["knora-admin:ProjectMember", "knora-admin:ProjectAdmin"],
+        "RV": ["knora-admin:ProjectMember", "knora-admin:KnownUser"],
+    }
+    expected = {
+        "CR": ["knora-admin:ProjectAdmin"],
+        "D": [],
+        "M": ["knora-admin:ProjectMember", "knora-admin:KnownUser"],
+        "V": [],
+        "RV": [],
+    }
+    assert expected == PermissionScope._remove_duplicates_from_kwargs(original)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main([__file__])
