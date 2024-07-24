e8bca17f412d8e0a764fe77aec11023f032b0c02
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-07-24 09:33:18 +0200

    merge 2 modules

--- dsp_permissions_scripts/oap/oap_get.py
+++ dsp_permissions_scripts/oap/oap_get.py
@@ -1,8 +1,10 @@
+import copy
+import re
 from typing import Any
+from urllib.parse import quote
 from urllib.parse import quote_plus
 
 from dsp_permissions_scripts.models.errors import ApiError
-from dsp_permissions_scripts.oap.oap_get_knora_base import get_oaps_of_kb_resclasses
 from dsp_permissions_scripts.oap.oap_model import Oap
 from dsp_permissions_scripts.oap.oap_model import OapRetrieveConfig
 from dsp_permissions_scripts.oap.oap_model import ResourceOap
@@ -31,6 +33,82 @@ IGNORE_KEYS = [
     "knora-api:userHasPermission",
     "knora-api:hasPermissions",
 ]
+KB_RESCLASSES = [
+    "knora-api:VideoSegment",
+    "knora-api:AudioSegment",
+    "knora-api:Region",
+    "knora-api:Annotation",
+    "knora-api:LinkObj",
+]
+
+
+def get_oaps_of_kb_resclasses(dsp_client: DspClient, project_iri: str, oap_config: OapRetrieveConfig) -> list[Oap]:
+    match oap_config.retrieve_resources:
+        case "none":
+            return []
+        case "specified_res_classes":
+            specified_kb_resclasses = [x for x in KB_RESCLASSES if x in oap_config.specified_res_classes]
+            res_only_oaps = _get_oaps_of_specified_kb_resclasses(dsp_client, project_iri, specified_kb_resclasses)
+        case "all":
+            res_only_oaps = _get_oaps_of_specified_kb_resclasses(dsp_client, project_iri, KB_RESCLASSES)
+
+    match oap_config.retrieve_values:
+        case "none":
+            return res_only_oaps
+        case "specified_props":
+            enriched_oaps = _enrich_with_value_oaps(dsp_client, res_only_oaps, oap_config.specified_props)
+        case "all":
+            enriched_oaps = _enrich_with_value_oaps(dsp_client, res_only_oaps)
+
+    return enriched_oaps
+
+
+def _get_oaps_of_specified_kb_resclasses(
+    dsp_client: DspClient, project_iri: str, kb_resclasses: list[str]
+) -> list[Oap]:
+    all_oaps: list[Oap] = []
+    for resclass in kb_resclasses:
+        all_oaps.extend(_get_oaps_of_one_kb_resclass(dsp_client, project_iri, resclass))
+    return all_oaps
+
+
+def _enrich_with_value_oaps(
+    dsp_client: DspClient, res_only_oaps: list[Oap], restrict_to_props: list[str] | None = None
+) -> list[Oap]:
+    complete_oaps = copy.deepcopy(res_only_oaps)
+    for oap in complete_oaps:
+        full_resource = dsp_client.get(f"/v2/resources/{quote_plus(oap.resource_oap.resource_iri)}")  # type: ignore[union-attr]
+        oap.value_oaps = get_value_oaps(full_resource, restrict_to_props)
+    return complete_oaps
+
+
+def _get_oaps_of_one_kb_resclass(dsp_client: DspClient, project_iri: str, resclass: str) -> list[Oap]:
+    oaps: list[Oap] = []
+    mayHaveMoreResults: bool = True
+    offset = 0
+    while mayHaveMoreResults:
+        sparql_query = """
+        PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+
+        CONSTRUCT {
+            ?linkobj knora-api:isMainResource true .
+        } WHERE {
+            BIND(<%(project_iri)s> as ?project_iri) .
+            ?linkobj a %(resclass)s .
+            ?linkobj knora-api:attachedToProject ?project_iri .
+        }
+        OFFSET %(offset)s
+        """ % {"resclass": resclass, "project_iri": project_iri, "offset": offset}  # noqa: UP031 (printf-string-formatting)
+        sparql_query_stripped = re.sub(r"\s+", " ", sparql_query).strip()
+        if not (response := dsp_client.get(f"/v2/searchextended/{quote(sparql_query_stripped, safe='')}")):
+            break  # if there are 0 results, the response is an empty dict
+        for json_resource in response.get("@graph", [response]):
+            scope = create_scope_from_string(json_resource["knora-api:hasPermissions"])
+            res_oap = ResourceOap(scope=scope, resource_iri=json_resource["@id"])
+            oaps.append(Oap(resource_oap=res_oap, value_oaps=[]))
+        mayHaveMoreResults = bool(response.get("knora-api:mayHaveMoreResults", False))
+        offset += 1
+    return oaps
 
 
 def _get_all_oaps_of_resclass(
--- tests/test_oap_get.py
+++ tests/test_oap_get.py
@@ -1,15 +1,18 @@
 from typing import Any
+from unittest.mock import Mock
 
 import pytest
 
 from dsp_permissions_scripts.models import group
 from dsp_permissions_scripts.models.scope import PermissionScope
 from dsp_permissions_scripts.oap.oap_get import _get_oap_of_one_resource
+from dsp_permissions_scripts.oap.oap_get import _get_oaps_of_one_kb_resclass
 from dsp_permissions_scripts.oap.oap_get import get_value_oaps
 from dsp_permissions_scripts.oap.oap_model import Oap
 from dsp_permissions_scripts.oap.oap_model import OapRetrieveConfig
 from dsp_permissions_scripts.oap.oap_model import ResourceOap
 from dsp_permissions_scripts.oap.oap_model import ValueOap
+from dsp_permissions_scripts.utils.dsp_client import DspClient
 
 
 @pytest.fixture()
@@ -189,3 +192,23 @@ def test_get_oap_of_one_resource_some_classes_some_values(resource: dict[str, An
     expected = Oap(resource_oap=expected_res_oap, value_oaps=[expected_val_oap])
     res = _get_oap_of_one_resource(resource, config)
     assert res == expected
+
+
+def test_get_oaps_of_one_kb_resclass_0_results() -> None:
+    dsp_client = Mock(spec=DspClient)
+    dsp_client.get = Mock(return_value={})
+    res = _get_oaps_of_one_kb_resclass(dsp_client, "proj_iri", "resclass")
+    assert res == []
+    assert len(dsp_client.get.call_args_list) == 1
+    called_route = dsp_client.get.call_args_list[0].args[0]
+    assert called_route.startswith("/v2/searchextended/")
+    assert "proj_iri" in called_route
+    assert "resclass" in called_route
+
+
+def test_get_oaps_of_one_kb_resclass_1_result() -> None:
+    pass
+
+
+def test_get_oaps_of_one_kb_resclass_4_results_on_2_pages() -> None:
+    pass
