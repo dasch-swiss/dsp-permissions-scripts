e7752e47f384ae9614ae1ae03bad510de3406592
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-10-17 15:49:37 +0200

    implement CustomGroup

--- dsp_permissions_scripts/models/group.py
+++ dsp_permissions_scripts/models/group.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
+import re
 from typing import Any
 from typing import Protocol
+from typing import Self
 
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import model_validator
 
 from dsp_permissions_scripts.models.errors import InvalidGroupError
+from dsp_permissions_scripts.utils.dsp_client import DspClient
 
 KNORA_ADMIN_ONTO_NAMESPACE = "http://www.knora.org/ontology/knora-admin#"
 
@@ -33,13 +36,37 @@ class BuiltinGroup(BaseModel):
         return data
 
     @model_validator(mode="after")
-    def _check_regex(self) -> BuiltinGroup:
+    def _check_regex(self) -> Self:
         if not self.val.startswith("knora-admin:"):
             raise InvalidGroupError(f"{self.val} is not a valid group IRI")
         return self
 
     def full_iri(self) -> str:
         return self.val.replace("knora-admin:", KNORA_ADMIN_ONTO_NAMESPACE)
+
+
+class CustomGroup(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    prefixed_iri: str
+    full_iri: str
+
+    @model_validator(mode="after")
+    def _check_regex(self) -> Self:
+        if not re.search(r"^\w+:\w+$", self.prefixed_iri):
+            raise InvalidGroupError(f"{self.prefixed_iri} is not a valid group IRI")
+        return self
+
+    @staticmethod
+    def from_unprefixed_groupname(string: str, dsp_client: DspClient, shortcode: str) -> CustomGroup:
+        all_groups = dsp_client.get("/admin/groups")["groups"]
+        proj_groups = [grp for grp in all_groups if grp["project"]["shortcode"] == shortcode]
+        group = [grp for grp in proj_groups if grp["name"] == string]
+        if not group:
+            msg = f"{string} is not a valid group. Available groups: {', '.join([grp['name'] for grp in proj_groups])}"
+            raise InvalidGroupError(msg)
+        prefixed_iri = f"{group[0]["project"]["shortname"]}:{string}"
+        return CustomGroup(prefixed_iri=prefixed_iri, full_iri=group[0]["id"])
 
 
 UNKNOWN_USER = BuiltinGroup(val="knora-admin:UnknownUser")
