37713bed12fea6cb1a65f6870faa80ca7040ca48
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2024-10-23 15:25:32 +0200

    pave the way for custom group, 3rd round (#150)

--- dsp_permissions_scripts/models/group_utils.py
+++ dsp_permissions_scripts/models/group_utils.py
@@ -26,9 +26,9 @@ def sort_groups(groups_original: Iterable[Group]) -> list[Group]:
     return groups
 
 
-def _get_sort_pos_of_custom_group(group: str) -> int:
+def _get_sort_pos_of_custom_group(prefixed_iri: str) -> int:
     alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    relevant_letter = group.replace("knora-admin:", "")[0]
+    relevant_letter = prefixed_iri.split(":")[-1][0]
     return alphabet.index(relevant_letter.lower()) + 99  # must be higher than the highest index of the builtin groups
 
 
--- tests/test_group_utils.py
+++ tests/test_group_utils.py
@@ -1,31 +1,37 @@
 import pytest
 
-from dsp_permissions_scripts.models import group
+from dsp_permissions_scripts.models.group import CREATOR
+from dsp_permissions_scripts.models.group import KNOWN_USER
+from dsp_permissions_scripts.models.group import PROJECT_ADMIN
+from dsp_permissions_scripts.models.group import PROJECT_MEMBER
+from dsp_permissions_scripts.models.group import SYSTEM_ADMIN
+from dsp_permissions_scripts.models.group import UNKNOWN_USER
+from dsp_permissions_scripts.models.group import Group
 from dsp_permissions_scripts.models.group_utils import sort_groups
 
 
 def test_sort_groups() -> None:
     groups_original = [
-        group.Group(prefixed_iri="knora-admin:C_CustomGroup"),
-        group.UNKNOWN_USER,
-        group.PROJECT_ADMIN,
-        group.PROJECT_MEMBER,
-        group.CREATOR,
-        group.Group(prefixed_iri="knora-admin:A_CustomGroup"),
-        group.Group(prefixed_iri="knora-admin:B_CustomGroup"),
-        group.KNOWN_USER,
-        group.SYSTEM_ADMIN,
+        Group(prefixed_iri="knora-admin:C_CustomGroup"),
+        UNKNOWN_USER,
+        PROJECT_ADMIN,
+        PROJECT_MEMBER,
+        CREATOR,
+        Group(prefixed_iri="knora-admin:A_CustomGroup"),
+        Group(prefixed_iri="knora-admin:B_CustomGroup"),
+        KNOWN_USER,
+        SYSTEM_ADMIN,
     ]
     groups_expected = [
-        group.SYSTEM_ADMIN,
-        group.CREATOR,
-        group.PROJECT_ADMIN,
-        group.PROJECT_MEMBER,
-        group.KNOWN_USER,
-        group.UNKNOWN_USER,
-        group.Group(prefixed_iri="knora-admin:A_CustomGroup"),
-        group.Group(prefixed_iri="knora-admin:B_CustomGroup"),
-        group.Group(prefixed_iri="knora-admin:C_CustomGroup"),
+        SYSTEM_ADMIN,
+        CREATOR,
+        PROJECT_ADMIN,
+        PROJECT_MEMBER,
+        KNOWN_USER,
+        UNKNOWN_USER,
+        Group(prefixed_iri="knora-admin:A_CustomGroup"),
+        Group(prefixed_iri="knora-admin:B_CustomGroup"),
+        Group(prefixed_iri="knora-admin:C_CustomGroup"),
     ]
     groups_returned = sort_groups(groups_original)
     assert groups_returned == groups_expected
