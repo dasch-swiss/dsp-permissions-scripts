ea58c070e1a2016e12a628c7609a4ad9fc28ea99
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-10-23 14:19:16 +0200

    rearrange

--- dsp_permissions_scripts/models/group_utils.py
+++ dsp_permissions_scripts/models/group_utils.py
@@ -9,12 +9,6 @@ from dsp_permissions_scripts.models.group import UNKNOWN_USER
 from dsp_permissions_scripts.models.group import Group
 
 
-def _get_sort_pos_of_custom_group(group: str) -> int:
-    alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    relevant_letter = group.replace("knora-admin:", "")[0]
-    return alphabet.index(relevant_letter.lower()) + 99  # must be higher than the highest index of the builtin groups
-
-
 def sort_groups(groups_original: Iterable[Group]) -> list[Group]:
     """
     Sorts groups:
@@ -25,3 +19,9 @@ def sort_groups(groups_original: Iterable[Group]) -> list[Group]:
     groups = list(groups_original)
     groups.sort(key=lambda x: sort_key.index(x) if x in sort_key else _get_sort_pos_of_custom_group(x.prefixed_iri))
     return groups
+
+
+def _get_sort_pos_of_custom_group(group: str) -> int:
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    relevant_letter = group.replace("knora-admin:", "")[0]
+    return alphabet.index(relevant_letter.lower()) + 99  # must be higher than the highest index of the builtin groups
