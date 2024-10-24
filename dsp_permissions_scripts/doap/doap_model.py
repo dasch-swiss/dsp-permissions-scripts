37de255a2b20200bf6675729352a239f09e9a137
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-10-24 16:09:46 +0200

    validators

--- dsp_permissions_scripts/doap/doap_model.py
+++ dsp_permissions_scripts/doap/doap_model.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 from typing import Self
 
 from pydantic import BaseModel
@@ -23,14 +24,29 @@ class GroupDoapTarget(BaseModel):
 
 
 class EntityDoapTarget(BaseModel):
+    """
+    Note that the resclass IRIs and property IRIs are in the environment-agnostic format
+    "http://www.knora.org/ontology/<shortcode>/<ontoname>#<classname_or_property_name>"
+    """
+
     project_iri: str
-    resource_class: str | None = None
-    property: str | None = None
+    resclass_iri: str | None = None
+    property_iri: str | None = None
 
     @model_validator(mode="after")
     def _validate(self) -> Self:
-        if self.resource_class is None and self.property is None:
+        if self.resclass_iri is None and self.property_iri is None:
             raise ValueError("At least one of resource_class or property must be set")
+        return self
+
+    @model_validator(mode="after")
+    def _validate_iri_format(self) -> Self:
+        rgx = r"http://www\.knora\.org/ontology/[A-Fa-f0-9]{4}/[^/#]+#[^/#]+"
+        iri_format = "http://www.knora.org/ontology/<shortcode>/<ontoname>#<classname_or_property_name>"
+        if self.resclass_iri and not re.search(rgx, self.resclass_iri):
+            raise ValueError(f"The IRI must be in the format '{iri_format}', but you provided {self.resclass_iri}")
+        if self.property_iri and not re.search(rgx, self.property_iri):
+            raise ValueError(f"The IRI must be in the format '{iri_format}', but you provided {self.property_iri}")
         return self
 
 
@@ -50,6 +66,10 @@ class NewEntityDoapTarget(BaseModel):
     def _validate(self) -> Self:
         if self.resclass_name is None and self.property_name is None:
             raise ValueError("At least one of resource_class or property must be set")
+        return self
+
+    @model_validator(mode="after")
+    def _validate_name_format(self) -> Self:
         if self.resclass_name and any([x in self.resclass_name for x in ["#", "/", "knora.org", "dasch.swiss"]]):
             raise ValueError(f"The resource class name must not be a full IRI, but you provided {self.resclass_name}")
         if self.property_name and any([x in self.property_name for x in ["#", "/", "knora.org", "dasch.swiss"]]):
