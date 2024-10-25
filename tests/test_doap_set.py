ac7a7873a5551ecc53291cd760c1de7743bdc065
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-10-25 10:17:42 +0200

    write unit test

--- tests/test_doap_set.py
+++ tests/test_doap_set.py
@@ -1,13 +1,19 @@
 from typing import Any
 from unittest.mock import Mock
+from unittest.mock import patch
 
 import pytest
 
 from dsp_permissions_scripts.doap import doap_set
+from dsp_permissions_scripts.doap.doap_model import NewEntityDoapTarget
 from dsp_permissions_scripts.doap.doap_model import NewGroupDoapTarget
 from dsp_permissions_scripts.doap.doap_set import create_new_doap_on_server
 from dsp_permissions_scripts.models import group
 from dsp_permissions_scripts.models.scope import PermissionScope
+
+SHORTCODE = "0000"
+ONTO_NAME = "limc"
+MY_RESCLASS_NAME = "MyResclass"
 
 
 @pytest.fixture
@@ -45,6 +51,41 @@ def response_for_group() -> dict[str, Any]:
     }
 
 
+@pytest.fixture
+def request_for_resclass() -> dict[str, Any]:
+    return {
+        "forGroup": None,
+        "forProject": "http://rdfh.ch/projects/P7Uo3YvDT7Kvv3EvLCl2tw",
+        "forProperty": None,
+        "forResourceClass": f"http://www.knora.org/ontology/{SHORTCODE}/{ONTO_NAME}#{MY_RESCLASS_NAME}",
+        "hasPermissions": [
+            {
+                "additionalInformation": "http://www.knora.org/ontology/knora-admin#UnknownUser",
+                "name": "V",
+                "permissionCode": None,
+            }
+        ],
+    }
+
+
+@pytest.fixture
+def response_for_resclass() -> dict[str, Any]:
+    return {
+        "default_object_access_permission": {
+            "iri": "http://rdfh.ch/permissions/4123/grKNPv-tQ7aBYq0mDXyatg",
+            "forProject": "http://rdfh.ch/projects/P7Uo3YvDT7Kvv3EvLCl2tw",
+            "forResourceClass": f"http://www.knora.org/ontology/{SHORTCODE}/{ONTO_NAME}#{MY_RESCLASS_NAME}",
+            "hasPermissions": [
+                {
+                    "name": "V",
+                    "additionalInformation": "http://www.knora.org/ontology/knora-admin#UnknownUser",
+                    "permissionCode": 2,
+                }
+            ],
+        }
+    }
+
+
 def test_create_doap_for_group(request_for_group: dict[str, Any], response_for_group: dict[str, Any]) -> None:
     doap_set.get_project_iri_and_onto_iris_by_shortcode = Mock(  # type: ignore[attr-defined]
         return_value=("http://rdfh.ch/projects/P7Uo3YvDT7Kvv3EvLCl2tw", None)
@@ -54,11 +95,35 @@ def test_create_doap_for_group(request_for_group: dict[str, Any], response_for_g
     dsp_client.post = Mock(return_value=response_for_group)
     _ = create_new_doap_on_server(
         target=NewGroupDoapTarget(group=group.KNOWN_USER),
-        shortcode="0000",
+        shortcode=SHORTCODE,
         scope=PermissionScope.create(V=[group.UNKNOWN_USER]),
         dsp_client=dsp_client,
     )
     dsp_client.post.assert_called_once_with("/admin/permissions/doap", data=request_for_group)
     doap_set.create_doap_from_admin_route_response.assert_called_once_with(  # type: ignore[attr-defined]
         response_for_group["default_object_access_permission"]
+    )
+
+
+@patch("dsp_permissions_scripts.doap.doap_set.create_doap_from_admin_route_response")
+@patch(
+    "dsp_permissions_scripts.doap.doap_set.get_project_iri_and_onto_iris_by_shortcode",
+    return_value=("http://rdfh.ch/projects/P7Uo3YvDT7Kvv3EvLCl2tw", None),
+)
+def test_create_doap_for_resclass(
+    get_project_iri_and_onto_iris_by_shortcode: Mock,  # noqa: ARG001
+    create_doap_from_admin_route_response: Mock,
+    request_for_resclass: dict[str, Any],
+    response_for_resclass: dict[str, Any],
+) -> None:
+    dsp_client = Mock(post=Mock(return_value=response_for_resclass))
+    _ = create_new_doap_on_server(
+        target=NewEntityDoapTarget(onto_name=ONTO_NAME, resclass_name=MY_RESCLASS_NAME),
+        shortcode=SHORTCODE,
+        scope=PermissionScope.create(V=[group.UNKNOWN_USER]),
+        dsp_client=dsp_client,
+    )
+    dsp_client.post.assert_called_once_with("/admin/permissions/doap", data=request_for_resclass)
+    create_doap_from_admin_route_response.assert_called_once_with(
+        response_for_resclass["default_object_access_permission"]
     )
