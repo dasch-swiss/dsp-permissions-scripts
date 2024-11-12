219d701c2be79d587ad2daada0638ee5ea753145
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-11-12 15:12:06 +0100

    add 1 more test

--- tests/test_doap_set.py
+++ tests/test_doap_set.py
@@ -13,6 +13,7 @@ from dsp_permissions_scripts.models.scope import PermissionScope
 SHORTCODE = "0000"
 ONTO_NAME = "limc"
 MY_RESCLASS_NAME = "MyResclass"
+MY_PROP_NAME = "hasProperty"
 PROJ_IRI = "http://rdfh.ch/projects/P7Uo3YvDT7Kvv3EvLCl2tw"
 HTTP_HOST = "http://api.dev.dasch.swiss"
 HTTPS_HOST = "https://api.dev.dasch.swiss"
@@ -88,6 +89,41 @@ def response_for_resclass() -> dict[str, Any]:
     }
 
 
+@pytest.fixture
+def request_for_prop() -> dict[str, Any]:
+    return {
+        "forGroup": None,
+        "forProject": PROJ_IRI,
+        "forResourceClass": None,
+        "forProperty": f"{HTTP_HOST}/ontology/{SHORTCODE}/{ONTO_NAME}/v2#{MY_PROP_NAME}",
+        "hasPermissions": [
+            {
+                "additionalInformation": "http://www.knora.org/ontology/knora-admin#UnknownUser",
+                "name": "V",
+                "permissionCode": None,
+            }
+        ],
+    }
+
+
+@pytest.fixture
+def response_for_prop() -> dict[str, Any]:
+    return {
+        "default_object_access_permission": {
+            "iri": "http://rdfh.ch/permissions/4123/grKNPv-tQ7aBYq0mDXyatg",
+            "forProject": PROJ_IRI,
+            "forProperty": f"{HTTP_HOST}/ontology/{SHORTCODE}/{ONTO_NAME}/v2#{MY_PROP_NAME}",
+            "hasPermissions": [
+                {
+                    "name": "V",
+                    "additionalInformation": "http://www.knora.org/ontology/knora-admin#UnknownUser",
+                    "permissionCode": 2,
+                }
+            ],
+        }
+    }
+
+
 @patch("dsp_permissions_scripts.doap.doap_set.create_doap_from_admin_route_response")
 @patch("dsp_permissions_scripts.doap.doap_set.get_proj_iri_and_onto_iris_by_shortcode", return_value=(PROJ_IRI, None))
 def test_create_doap_for_group(
@@ -127,4 +163,25 @@ def test_create_doap_for_resclass(
     dsp_client.post.assert_called_once_with("/admin/permissions/doap", data=request_for_resclass)
     create_doap_from_admin_route_response.assert_called_once_with(
         response_for_resclass["default_object_access_permission"], dsp_client
+    )
+
+
+@patch("dsp_permissions_scripts.doap.doap_set.create_doap_from_admin_route_response")
+@patch("dsp_permissions_scripts.doap.doap_set.get_proj_iri_and_onto_iris_by_shortcode", return_value=(PROJ_IRI, None))
+def test_create_doap_for_prop(
+    get_project_iri_and_onto_iris_by_shortcode: Mock,  # noqa: ARG001
+    create_doap_from_admin_route_response: Mock,
+    request_for_prop: dict[str, Any],
+    response_for_prop: dict[str, Any],
+) -> None:
+    dsp_client = Mock(post=Mock(return_value=response_for_prop), server=HTTPS_HOST)
+    _ = create_new_doap_on_server(
+        target=NewEntityDoapTarget(prefixed_prop=f"{ONTO_NAME}:{MY_PROP_NAME}"),
+        shortcode=SHORTCODE,
+        scope=PermissionScope.create(V=[group.UNKNOWN_USER]),
+        dsp_client=dsp_client,
+    )
+    dsp_client.post.assert_called_once_with("/admin/permissions/doap", data=request_for_prop)
+    create_doap_from_admin_route_response.assert_called_once_with(
+        response_for_prop["default_object_access_permission"], dsp_client
     )
