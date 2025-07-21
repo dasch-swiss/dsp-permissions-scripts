84666ff939636d83e62a944c8403639ce93fee6a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-07-21 21:45:39 +0200

    wip

--- dsp_permissions_scripts/0102_scenario_tanner_group_aps.py
+++ dsp_permissions_scripts/0102_scenario_tanner_group_aps.py
@@ -1,9 +1,23 @@
+import copy
+
 from dsp_permissions_scripts.ap.ap_get import get_aps_of_project
 from dsp_permissions_scripts.ap.ap_model import ApValue
 from dsp_permissions_scripts.ap.ap_serialize import serialize_aps_of_project
 from dsp_permissions_scripts.ap.ap_set import create_new_ap_on_server
+from dsp_permissions_scripts.doap.doap_get import get_doaps_of_project
+from dsp_permissions_scripts.doap.doap_model import Doap
+from dsp_permissions_scripts.doap.doap_model import GroupDoapTarget
+from dsp_permissions_scripts.doap.doap_serialize import serialize_doaps_of_project
+from dsp_permissions_scripts.doap.doap_set import apply_updated_scopes_of_doaps_on_server
+from dsp_permissions_scripts.models.group import PROJECT_ADMIN
 from dsp_permissions_scripts.models.group import CustomGroup
 from dsp_permissions_scripts.models.host import Hosts
+from dsp_permissions_scripts.models.scope import OPEN
+from dsp_permissions_scripts.oap.oap_get import get_all_oaps_of_project
+from dsp_permissions_scripts.oap.oap_model import ModifiedOap
+from dsp_permissions_scripts.oap.oap_model import Oap
+from dsp_permissions_scripts.oap.oap_model import OapRetrieveConfig
+from dsp_permissions_scripts.oap.oap_set import apply_updated_oaps_on_server
 from dsp_permissions_scripts.utils.authentication import login
 from dsp_permissions_scripts.utils.dsp_client import DspClient
 from dsp_permissions_scripts.utils.get_logger import get_logger
@@ -36,15 +50,88 @@ def update_aps(shortcode: str, dsp_client: DspClient) -> None:
     )
 
 
+def modify_doaps(doaps: list[Doap]) -> list[Doap]:
+    """Adapt this sample to your needs."""
+    modified_doaps = []
+    for doap in copy.deepcopy(doaps):
+        if isinstance(doap.target, GroupDoapTarget) and doap.target.group == PROJECT_ADMIN:
+            doap.scope = OPEN
+            modified_doaps.append(doap)
+    return modified_doaps
+
+
+def modify_oaps(oaps: list[Oap]) -> list[ModifiedOap]:
+    """Adapt this sample to your needs."""
+    modified_oaps: list[ModifiedOap] = []
+    tanner_group = CustomGroup(prefixed_iri="scenario-tanner:group-scenario-tanner")
+    for oap in copy.deepcopy(oaps):
+        new_oap = ModifiedOap()
+        if tanner_group not in oap.resource_oap.scope.M:
+            new_scope = oap.resource_oap.scope.add(permission="M", group=tanner_group)
+            new_oap.resource_oap = oap.resource_oap.model_copy(update={"scope": new_scope})
+        if not new_oap.is_empty():
+            modified_oaps.append(new_oap)
+    return modified_oaps
+
+
+def update_doaps(shortcode: str, dsp_client: DspClient) -> None:
+    """Sample function to modify the Default Object Access Permissions of a project."""
+    project_doaps = get_doaps_of_project(shortcode, dsp_client)
+    serialize_doaps_of_project(
+        project_doaps=project_doaps,
+        shortcode=shortcode,
+        mode="original",
+        server=dsp_client.server,
+    )
+    project_doaps_modified = modify_doaps(doaps=project_doaps)
+    if not project_doaps_modified:
+        logger.info("There are no DOAPs to update.")
+        return
+    apply_updated_scopes_of_doaps_on_server(project_doaps_modified, dsp_client)
+    project_doaps_updated = get_doaps_of_project(shortcode, dsp_client)
+    serialize_doaps_of_project(
+        project_doaps=project_doaps_updated,
+        shortcode=shortcode,
+        mode="modified",
+        server=dsp_client.server,
+    )
+
+
+def update_oaps(shortcode: str, dsp_client: DspClient, oap_config: OapRetrieveConfig) -> None:
+    """Sample function to modify the Object Access Permissions of a project."""
+    oaps = get_all_oaps_of_project(shortcode, dsp_client, oap_config)
+    oaps_modified = modify_oaps(oaps)
+    if not oaps_modified:
+        logger.info("There are no OAPs to update.")
+        return
+    apply_updated_oaps_on_server(
+        oaps=oaps_modified,
+        shortcode=shortcode,
+        dsp_client=dsp_client,
+        nthreads=4,
+    )
+
+
 def main() -> None:
     host = Hosts.get_host("rdu")
     shortcode = "0102"
     log_start_of_script(host, shortcode)
     dsp_client = login(host)
 
+    oap_config = OapRetrieveConfig(retrieve_resources="all", retrieve_values="none")
+
     update_aps(
         shortcode=shortcode,
         dsp_client=dsp_client,
+    )
+    update_doaps(
+        shortcode=shortcode,
+        dsp_client=dsp_client,
+    )
+    update_oaps(
+        shortcode=shortcode,
+        dsp_client=dsp_client,
+        oap_config=oap_config,
     )
 
 
