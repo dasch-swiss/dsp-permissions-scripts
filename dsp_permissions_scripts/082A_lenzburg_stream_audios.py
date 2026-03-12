9aca8bf04a15c3d84c0471f096cf028f5f630ccd
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2026-03-12 16:50:57 +0100

    probably successful on stage? check when NFS problems are over

--- dsp_permissions_scripts/082A_lenzburg_stream_audios.py
+++ dsp_permissions_scripts/082A_lenzburg_stream_audios.py
@@ -1,18 +1,11 @@
 import copy
 
-from dsp_permissions_scripts.doap.doap_get import get_doaps_of_project
-from dsp_permissions_scripts.doap.doap_model import Doap
-from dsp_permissions_scripts.doap.doap_model import GroupDoapTarget
-from dsp_permissions_scripts.doap.doap_serialize import serialize_doaps_of_project
-from dsp_permissions_scripts.models import group
 from dsp_permissions_scripts.models.host import Hosts
 from dsp_permissions_scripts.models.scope import LIMITED_VIEW
-from dsp_permissions_scripts.models.scope import PUBLIC
 from dsp_permissions_scripts.oap.oap_get import get_all_oaps_of_project
 from dsp_permissions_scripts.oap.oap_model import ModifiedOap
 from dsp_permissions_scripts.oap.oap_model import Oap
 from dsp_permissions_scripts.oap.oap_model import OapRetrieveConfig
-from dsp_permissions_scripts.oap.oap_serialize import serialize_oaps
 from dsp_permissions_scripts.oap.oap_set import apply_updated_oaps_on_server
 from dsp_permissions_scripts.utils.authentication import login
 from dsp_permissions_scripts.utils.dsp_client import DspClient
@@ -20,16 +13,6 @@ from dsp_permissions_scripts.utils.get_logger import get_logger
 from dsp_permissions_scripts.utils.get_logger import log_start_of_script
 
 logger = get_logger(__name__)
-
-
-def modify_doaps(doaps: list[Doap]) -> list[Doap]:
-    """Adapt this sample to your needs."""
-    modified_doaps = []
-    for doap in copy.deepcopy(doaps):
-        if isinstance(doap.target, GroupDoapTarget) and doap.target.group == group.PROJECT_ADMIN:
-            doap.scope = PUBLIC
-            modified_doaps.append(doap)
-    return modified_doaps
 
 
 def modify_oaps(oaps: list[Oap]) -> list[ModifiedOap]:
@@ -47,44 +30,9 @@ def modify_oaps(oaps: list[Oap]) -> list[ModifiedOap]:
     return modified_oaps
 
 
-def update_doaps(shortcode: str, dsp_client: DspClient) -> None:
-    """Sample function to modify the Default Object Access Permissions of a project."""
-    project_doaps = get_doaps_of_project(shortcode, dsp_client)
-    serialize_doaps_of_project(
-        project_doaps=project_doaps,
-        shortcode=shortcode,
-        mode="original",
-        server=dsp_client.server,
-    )
-    # remaining_doaps = delete_doap_of_group_on_server(
-    #     existing_doaps=project_doaps,
-    #     forGroup=group.PROJECT_MEMBER,
-    #     dsp_client=dsp_client,
-    # )
-    # _ = create_new_doap_on_server(
-    #     target=NewGroupDoapTarget(group=group.CREATOR),
-    #     shortcode=shortcode,
-    #     scope=PermissionScope.create(CR=[group.SYSTEM_ADMIN]),
-    #     dsp_client=dsp_client,
-    # )
-    # project_doaps_modified = modify_doaps(doaps=remaining_doaps)
-    # if not project_doaps_modified:
-    #     logger.info("There are no DOAPs to update.")
-    #     return
-    # apply_updated_scopes_of_doaps_on_server(project_doaps_modified, dsp_client)
-    # project_doaps_updated = get_doaps_of_project(shortcode, dsp_client)
-    # serialize_doaps_of_project(
-    #     project_doaps=project_doaps_updated,
-    #     shortcode=shortcode,
-    #     mode="modified",
-    #     server=dsp_client.server,
-    # )
-
-
 def update_oaps(shortcode: str, dsp_client: DspClient, oap_config: OapRetrieveConfig) -> None:
     """Sample function to modify the Object Access Permissions of a project."""
     oaps = get_all_oaps_of_project(shortcode, dsp_client, oap_config)
-    serialize_oaps(oaps, shortcode, mode="original")
     oaps_modified = modify_oaps(oaps)
     if not oaps_modified:
         logger.info("There are no OAPs to update.")
@@ -93,10 +41,8 @@ def update_oaps(shortcode: str, dsp_client: DspClient, oap_config: OapRetrieveCo
         oaps=oaps_modified,
         shortcode=shortcode,
         dsp_client=dsp_client,
-        nthreads=4,
+        nthreads=2,
     )
-    oaps_updated = get_all_oaps_of_project(shortcode, dsp_client, oap_config)
-    serialize_oaps(oaps_updated, shortcode, mode="modified")
 
 
 def main() -> None:
@@ -116,7 +62,6 @@ def main() -> None:
         retrieve_resources="specified_res_classes",
         specified_res_classes=["sva:AudioRepresentation"],
         retrieve_values="all",
-        # specified_props=["knora-api:hasAudioFileValue"],
     )
 
     update_oaps(
@@ -124,11 +69,6 @@ def main() -> None:
         dsp_client=dsp_client,
         oap_config=oap_config,
     )
-
-    # update_doaps(
-    #     shortcode=shortcode,
-    #     dsp_client=dsp_client,
-    # )
 
 
 if __name__ == "__main__":
